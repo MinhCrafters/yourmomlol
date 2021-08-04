c88867bcc0a4fe1899be8313c7fbdddca7b29117
Minh Le <levan@DESKTOP-57OGPA2.(none)>
2021-08-04 18:00:28 +0700

    lol

--- server.js
+++ server.js
@@ -2,13 +2,45 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const formatMessage = require('./utils/messages.js');
-const {
-    userJoin,
-    getCurrentUser,
-    userLeave,
-    getRoomUsers
-} = require('./utils/users.js');
+const moment = require('moment');
+
+function formatMessage(username, text) {
+    return {
+        username,
+        text,
+        time: moment().format('h:mm a')
+    };
+}
+
+const users = [];
+
+// Join user to chat
+function userJoin(id, username, room) {
+    const user = { id, username, room };
+
+    users.push(user);
+
+    return user;
+}
+
+// Get current user
+function getCurrentUser(id) {
+    return users.find(user => user.id === id);
+}
+
+// User leaves chat
+function userLeave(id) {
+    const index = users.findIndex(user => user.id === id);
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0];
+    }
+}
+
+// Get room users
+function getRoomUsers(room) {
+    return users.filter(user => user.room === room);
+}
 
 const app = express();
 const server = http.createServer(app);
