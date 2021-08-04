b031e96508478916cf220698113225a0c8c22bf8
Minh Le <levan@DESKTOP-57OGPA2.(none)>
2021-08-04 17:16:56 +0700

    make it better

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const formatMessage = require('./utils/messages');
+const formatMessage = require('./utils/messages.js');
 const {
     userJoin,
     getCurrentUser,
     userLeave,
     getRoomUsers
-} = require('./utils/users');
+} = require('./utils/users.js');
 
 const app = express();
 const server = http.createServer(app);
